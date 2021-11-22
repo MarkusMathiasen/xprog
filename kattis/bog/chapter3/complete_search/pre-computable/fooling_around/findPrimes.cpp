713fdd7353c982d2e677f03849e3599e0361faf4
Markus Engelund Mathiasen <202005875@post.au.dk>
2021-11-22 09:26:12 +0100

    first three chapter3 problems solved

---
+++ kattis/bog/chapter3/complete_search/pre-computable/fooling_around/findPrimes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N = 1e9+1;
+
+vector<bool> isPrime(N, true);
+vector<int> primesMinusOne;
+
+int main() {
+	isPrime[0] = isPrime[1] = false;
+	for (int i = 2; i <= ceil(sqrt(N)); i++)
+		if (isPrime[i])
+			for (int j = i*i; j <= N; j += i)
+				isPrime[j] = false;
+	for (int i = 0; i <= N; i++)
+		if (isPrime[i])
+			primesMinusOne.push_back(i-1);
+	for (int& x : primesMinusOne)
+		printf("%d\n", x);
+}
