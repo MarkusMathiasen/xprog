b6cd1ebc0fd6b2119bb05c6ce24da32b1943fb1f
Markus Engelund Mathiasen <202005875@post.au.dk>
2021-11-26 19:22:54 +0100

    did something?

--- kattis/bog/chapter3/complete_search/pre-computable/fooling_around/findPrimes.cpp
+++ kattis/bog/chapter3/complete_search/pre-computable/fooling_around/findPrimes.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+<<<<<<< HEAD
 int N = 1e9;
 
 bitset<1000000001> isNotPrime;
@@ -15,4 +16,22 @@ int main() {
 	for (int i = 0; i <= N; i++)
 		if (!isNotPrime[i])
 			printf("%d\n", i);
+=======
+int N = 1e9+1;
+
+vector<bool> isPrime(N, true);
+vector<int> primesMinusOne;
+
+int main() {
+	isPrime[0] = isPrime[1] = false;
+	for (int i = 2; i <= ceil(sqrt(N)); i++)
+		if (isPrime[i])
+			for (int j = i*i; j <= N; j += i)
+				isPrime[j] = false;
+	for (int i = 0; i <= N; i++)
+		if (isPrime[i])
+			primesMinusOne.push_back(i-1);
+	for (int& x : primesMinusOne)
+		printf("%d\n", x);
+>>>>>>> 5fd5269b77be81de3efc84f97edb5ceade7c18ea
 }
