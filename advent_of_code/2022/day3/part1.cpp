147f99ee62303540d62ccb7db83e1ddf58fba6fb
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-03 06:22:33 +0100

    Day 3 accepted

--- advent_of_code/2022/day3/part1.cpp
+++ advent_of_code/2022/day3/part1.cpp
@@ -11,4 +11,23 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
+	string line1, line2, line3;
+	int res = 0;
+	while (cin >> line1 >> line2 >> line3) {
+		set<char> s1, s2, s3;
+		for (char c : line1)
+			s1.insert(c);
+		for (char c : line2)
+			if (s1.count(c))
+				s2.insert(c);
+		for (char c : line3)
+			if (s2.count(c))
+				s3.insert(c);
+		char common = *s3.begin();
+		if (islower(common))
+			res += common -'a'+1;
+		else
+			res += common-'A'+27;
+	}
+	printf("%d\n", res);
 }
