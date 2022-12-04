79aae5ffca44d949730994cdd9bbd9cc7e81e318
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-04 06:09:36 +0100

    Day 4 accepted

--- advent_of_code/2022/day5/main.cpp
+++ advent_of_code/2022/day5/main.cpp
@@ -11,13 +11,4 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
-	int a, b, c, d;
-	int res = 0;
-	while (scanf("%d-%d,%d-%d", &a, &b, &c, &d) != EOF) {
-		if (a <= c && b >= d || c <= a && d >= b)
-			res++;
-		else if (a <= c && b >= c || a <= d && b >= d)
-			res++;
-	}
-	printf("%d\n", res);
 }
