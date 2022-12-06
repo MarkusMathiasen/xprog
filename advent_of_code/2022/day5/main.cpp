866e79c2d8402d692552ef9c661f1d7c1f8e047e
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-06 05:57:19 +0100

    Day 5 accepted

--- advent_of_code/2022/day5/main.cpp
+++ advent_of_code/2022/day5/main.cpp
@@ -11,4 +11,30 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
+	int n; cin >> n;
+	string line;
+	vector<deque<char>> A(n, deque<char>());
+	getline(cin, line);
+	while (getline(cin, line), line[1] != '1') {
+		rep(i, 0, n) {
+			if (isalpha(line[4*i+1]))
+				A[i].push_back(line[4*i+1]);
+		}
+	}
+	int k, a, b;
+	while (cin >> line >> k >> line >> a >> line >> b) {
+		a--, b--;
+		deque<char> buf;
+		rep(i, 0, k) {
+			buf.push_front(A[a].front());
+			A[a].pop_front();
+		}
+		rep(i, 0, k) {
+			A[b].push_front(buf.front());
+			buf.pop_front();
+		}
+	}
+	for (deque<char> s : A)
+		printf("%c", s.front());
+	printf("\n");
 }
