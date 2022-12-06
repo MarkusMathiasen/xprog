4eeab3a9819cbfa7b9e387c8d21fe039b639ace6
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-06 06:09:07 +0100

    Day 6 accepted

--- advent_of_code/2022/day7/main.cpp
+++ advent_of_code/2022/day7/main.cpp
@@ -3,24 +3,12 @@ using namespace std;
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
+#define sz(x) (in)(x).size()
+typedef long long ;;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
-	string line;
-	getline(cin, line);
-	rep(i, 13, sz(line)) {
-		set<char> s;
-		rep(j, i-13, i+1) {
-			s.insert(line[j]);
-		}
-		if (sz(s) == 14) {
-			printf("%d\n", i+1);
-			return 0;
-		}
-	}
 }
