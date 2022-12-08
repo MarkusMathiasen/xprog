d94a92a13a64df0e39cbd38385dc02ce08f77d51
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-08 06:30:19 +0100

    Day 8 accepted

--- advent_of_code/2022/day7/main.cpp
+++ advent_of_code/2022/day7/main.cpp
@@ -8,56 +8,51 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-map<string, vector<string>> graph;
-unordered_map<string, ll> w;
+map<string, int> w;
+unordered_map<string, vector<string>> graph;
 
-ll update_w(string v) {
-	for (string& u : graph[v]) {
-		w[v] += update_w(u);
-	}
-	return w[v];
+void compute_w(string v) {
+	for (string& u : graph[v])
+		compute_w(u),
+		w[v] += w[u];
 }
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
-	string line;
-	string dir = "";
+	string line, dir = "";
 	getline(cin, line);
-	while (line != "") {
+	while (sz(line)) {
 		stringstream ss(line);
-		string dollar, cmd;
-		ss >> dollar >> cmd;
-		if (cmd == "ls") {
-			ll sum = 0;
-			getline(cin, line);
-			while (sz(line) > 0 && line[0] != '$') {
-				stringstream s2(line);
-				string a, b; s2 >> a >> b;
-				if (a == "dir") {
-					graph[dir].push_back(dir + b + "/");
-				} else {
-					sum += stoll(a);
-				}
-				getline(cin, line);
-			}
-			w[dir] = sum;
-		} else if (cmd == "cd") {
-			string d; ss >> d;
-			if (d == "..") {
+		string cmd, arg; ss >> cmd >> cmd;
+		if (cmd == "cd") {
+			ss >> arg;
+			if (arg == "..") {
 				dir.pop_back();
 				while (dir.back() != '/')
 					dir.pop_back();
 			} else
-				dir += (d == "/" ? "" : d) + "/";
-			getline(cin, line);
+				dir += sz(dir) ? arg + "/" : arg;
+		} else if (cmd == "ls") {
+			bool update = !w[dir];
+			while (getline(cin, line), sz(line) && line[0] != '$') {
+				if (update) {
+					stringstream s2(line);
+					string a, b; s2 >> a >> b;
+					if (a == "dir")
+						graph[dir].push_back(dir + b + "/");
+					else
+						w[dir] += stoi(a);
+				}
+			}
+			continue;
 		} else assert(false);
+		getline(cin, line);
 	}
-	update_w("/");
-	ll res = 1e18;
-	ll target = 30000000 - (70000000 - w["/"]);
-	for (auto& [v,d] : w)
-		if (d >= target)
-			res = min(res, d);
-	printf("%lld\n", res);
+	int res = 1e9;
+	compute_w("/");
+	for (auto& [k,v] : w)
+		if (w["/"] - v <= 40000000)
+			res = min(res, v);
+	printf("%d\n", res);
 }
--- advent_of_code/2022/day8/main.cpp
+++ advent_of_code/2022/day8/main.cpp
@@ -11,4 +11,32 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
+	vector<string> A;
+	string line;
+	while (cin >> line)
+		A.push_back(line);
+	vector<vector<int>> vis(sz(A), vector<int>(sz(A[0]), 1));
+	int res = 0;
+	rep(i, 1, sz(A)-1)
+		rep(j, 1, sz(A[i])-1) {
+			int v = 1;
+			int j2 = j-1;
+			while (j2 > 0 && A[i][j] > A[i][j2])
+				j2--;
+			v *= j-j2;
+			j2 = j+1;
+			while (j2+1<sz(A[i]) && A[i][j] > A[i][j2])
+				j2++;
+			v *= j2-j;
+			int i2 = i-1;
+			while (i2 > 0 && A[i][j] > A[i2][j])
+				i2--;
+			v *= i-i2;
+			i2 = i+1;
+			while (i2+1<sz(A) && A[i][j] > A[i2][j])
+				i2++;
+			v *= i2-i;
+			res = max(res, v);
+		}
+	printf("%d\n", res);
 }
