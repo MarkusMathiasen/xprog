cc248b90e93d60f49a1efb617eb48d7e1b1c49a4
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-10 06:28:57 +0100

    Day 10 accepted

--- advent_of_code/2022/day10/main.cpp
+++ advent_of_code/2022/day10/main.cpp
@@ -11,4 +11,35 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
+	int x = 1;
+	int cycle = 0;
+	string s; int k;
+	int res = 0;
+	vector<string> A;
+	while (cin >> s) {
+		if ((cycle) % 40 == 0) {
+			res += x*cycle;
+			A.push_back("");
+		}
+		A.back().push_back(abs(cycle%40-x) <= 1 ? '#' : '.');
+		if (s == "addx") {
+			cin >> k;
+			cycle++;
+			if (cycle % 40 == 0) {
+				res += x*cycle;
+				A.push_back("");
+			}
+			A.back().push_back(abs(cycle%40-x) <= 1 ? '#' : '.');
+			x += k;
+			cycle++;
+		} else if (s == "noop") {
+			cycle++;
+		}
+	}
+	if ((x-20) % 40 == 0) {
+		res += k*cycle;
+		A.push_back("");
+	}
+	for (string& s : A)
+		printf("%s\n", s.c_str());
 }
