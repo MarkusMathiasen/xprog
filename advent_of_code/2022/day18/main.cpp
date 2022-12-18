009efa65bd59878ffabc3c8f972aa33e8e0e3e8f
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-18 06:21:57 +0100

    Day 18 accepted

--- advent_of_code/2022/day18/main.cpp
+++ advent_of_code/2022/day18/main.cpp
@@ -11,4 +11,54 @@ typedef vector<int> vi;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
+    set<vi> sides;
+    set<vi> cubes;
+    int max_c = 0;
+    int x,y,z; while (scanf("%d,%d,%d", &x, &y, &z) != EOF) {
+        cubes.insert({x,y,z});
+        max_c = max(max_c, max(x, max(y, z)));
+        rep(i, 0, 3)
+            if (!sides.count({x, y, z, i}))
+                sides.insert({x, y, z, i});
+            else
+                sides.erase({x, y, z, i});
+        rep(i, 0, 3)
+            if (!sides.count({x - (i == 0), y - (i == 1), z - (i == 2), i}))
+                sides.insert({x - (i == 0), y - (i == 1), z - (i == 2), i});
+            else
+                sides.erase({x - (i == 0), y - (i == 1), z - (i == 2), i});
+    }
+    queue<vi> q;
+    vector<vector<vi>> vis(max_c+2, vector<vi>(max_c+2, vi(max_c+2, 0)));
+    q.push({0,0,0});
+    for (vi cube : cubes) {
+        vis[cube[0]][cube[1]][cube[2]] = 1;
+    }
+    while (!q.empty()) {
+        vi cur = q.front(); q.pop();
+        int x = cur[0], y = cur[1], z = cur[2];
+        if (x < 0 || x > max_c+1 || y < 0 || y > max_c+1 || z < 0 || z > max_c+1) continue;
+        if (vis[x][y][z]) continue;
+        vis[x][y][z] = 1;
+        vi dx = {-1, 1, 0, 0, 0, 0};
+        vi dy = {0, 0, -1, 1, 0, 0};
+        vi dz = {0, 0, 0, 0, -1, 1};
+        rep(i, 0, 6) {
+            q.push({x+dx[i], y+dy[i], z+dz[i]});
+        }
+    }
+    rep(x, 0, max_c+2) {
+        rep(y, 0, max_c+2) {
+            rep(z, 0, max_c+2) {
+                if (!vis[x][y][z]) {
+                    rep(i, 0, 3)
+                        sides.erase({x, y, z, i});
+                    rep(i, 0, 3)
+                        sides.erase({x - (i == 0), y - (i == 1), z - (i == 2), i});
+                }
+            }
+        }
+    }
+    printf("%ld\n", sides.size());
+
 }
