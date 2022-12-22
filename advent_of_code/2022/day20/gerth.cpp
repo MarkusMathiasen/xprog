95da5c2a7acb2e64e271c0485af20e6a458f8dec
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-22 05:58:41 +0100

    Day 20 extra

---
+++ advent_of_code/2022/day20/gerth.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(long long i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+const ll mul = 811589153ll;
+
+struct element {
+    ll val, nxt, prv;
+};
+
+vector<element> v;
+
+void move_num(vector<element>::iterator it) {
+    pii val = *it;
+    it = v.erase(it);
+    ll i = (ll)(it - v.begin());
+    ll idx = (i+val.first%sz(v) + sz(v))%sz(v);
+    v.insert(v.begin()+idx, val);
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	//cin.exceptions(cin.failbit);
+    ll cnt = 0;
+    ll x; while (scanf("%lld", &x) == 1)
+        v.emplace_back(x*mul, cnt++);
+    rep(j, 0, 10) {
+        printf("Start iteration: %lld\n", j);
+        rep(i, 0, sz(v)) {
+            auto it = v.begin();
+            while (it->second != i) it++;
+            move_num(it);
+        }
+    }
+    ll zero_pos;
+    rep(i, 0, sz(v)) {
+        if (v[i].first == 0) {
+            zero_pos = i;
+            break;
+        }
+    }
+    ll res = 0;
+    rep(i, 1, 3001) {
+        if (i % 1000 == 0)
+            printf("%lld ", v[(i+zero_pos)%sz(v)].first),
+            res += v[(i+zero_pos)%sz(v)].first;
+    }
+    printf("\n");
+    printf("%lld\n", res);
+}
