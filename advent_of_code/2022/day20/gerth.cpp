3dda5acc8761e35fa9a012eb4d4e2fd41eb7133b
Markus Mathiasen <markusemathiasen@gmail.com>
2022-12-23 05:59:25 +0100

    Gerth done

--- advent_of_code/2022/day20/gerth.cpp
+++ advent_of_code/2022/day20/gerth.cpp
@@ -8,46 +8,63 @@ typedef long long ll;
 typedef pair<ll, ll> pii;
 const ll mul = 811589153ll;
 
-struct element {
-    ll val, nxt, prv;
-};
+vector<ll> v, val;
 
-vector<element> v;
-
-void move_num(vector<element>::iterator it) {
-    pii val = *it;
+void move_num(vector<ll>::iterator it) {
+    ll num = *it;
+    ll am = val[num];
     it = v.erase(it);
     ll i = (ll)(it - v.begin());
-    ll idx = (i+val.first%sz(v) + sz(v))%sz(v);
-    v.insert(v.begin()+idx, val);
+    ll idx = (i+am%sz(v) + sz(v))%sz(v);
+    v.insert(v.begin()+idx, num);
+}
+/*
+void print_list() {
+    rep(i, 0, sz(v)) {
+        printf("%lld ", val[v[i]]);
+    }
+    printf("\n");
 }
 
+vector<ll>::iterator search_near(int val, int guess_idx) {
+    rep(i, 0, sz(v)/2+1) {
+        int idx1 = (guess_idx+i)%sz(v);
+        int idx2 = (guess_idx-i+sz(v))%sz(v);
+        if (v[idx1] == val)
+            return v.begin()+idx1;
+        else if (v[idx2] == val)
+            return v.begin()+idx2;
+    }
+    return v.end();
+}
+*/
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//cin.exceptions(cin.failbit);
-    ll cnt = 0;
-    ll x; while (scanf("%lld", &x) == 1)
-        v.emplace_back(x*mul, cnt++);
+    ll cnt = 0, zero_cnt = -1;
+    ll x; while (scanf("%lld", &x) == 1) {
+        v.push_back(cnt++);
+        val.push_back(x*mul);
+        if (x == 0 && zero_cnt == -1) zero_cnt = cnt-1;
+    }
+    ll og_sz = sz(v);
+    rep(j, 0, 200) rep(i, 0, og_sz)
+        v.push_back(cnt++), val.push_back(val[i]);
     rep(j, 0, 10) {
         printf("Start iteration: %lld\n", j);
         rep(i, 0, sz(v)) {
-            auto it = v.begin();
-            while (it->second != i) it++;
+            if (i % 100000 == 0)
+                printf("i = %d\n", i);
+            auto it = find(all(v), i);
             move_num(it);
         }
     }
-    ll zero_pos;
-    rep(i, 0, sz(v)) {
-        if (v[i].first == 0) {
-            zero_pos = i;
-            break;
-        }
-    }
+    ll zero_pos = find(all(v), zero_cnt) - v.begin();
     ll res = 0;
     rep(i, 1, 3001) {
         if (i % 1000 == 0)
-            printf("%lld ", v[(i+zero_pos)%sz(v)].first),
-            res += v[(i+zero_pos)%sz(v)].first;
+            printf("%lld ", val[v[(i+zero_pos)%sz(v)]]),
+            res += val[v[(i+zero_pos)%sz(v)]];
     }
     printf("\n");
     printf("%lld\n", res);
